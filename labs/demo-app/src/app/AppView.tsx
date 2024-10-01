95e9d5ec732b69fb3f54e501b0656ebbd384804b
waynesbrain <175370544+waynesbrain@users.noreply.github.com>
2024-09-30 22:15:41 -0400

    Notes

--- labs/demo-app/src/app/AppView.tsx
+++ labs/demo-app/src/app/AppView.tsx
@@ -9,9 +9,28 @@ export function AppView() {
       console.log("AppView: Started.");
     });
   }, []);
-  return <div>Hello</div>;
+  return (
+    <div style={styles.root}>
+      <h1>Hello</h1>
+      <p>&nbsp;</p>
+      <p>Things are happening in the devtools right now...</p>
+    </div>
+  );
 }
 
 export type AppView = typeof AppView;
 
 export default AppView;
+
+const styles = {
+  root: {
+    alignItems: "center",
+    color: "gainsboro",
+    background: "linear-gradient(0deg, royalblue, darkorchid)",
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    justifyContent: "center",
+    width: "100vw",
+  },
+} satisfies Record<string, React.CSSProperties>;
