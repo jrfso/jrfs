7fa68634461b55bfe3be3303c1a51e6aa60b0556
waynesbrain <175370544+waynesbrain@users.noreply.github.com>
2024-10-23 12:45:31 -0400

    Register simple-git command stubs for test.

--- labs/demo-server/src/lib/jrfs/simple-git.ts
+++ labs/demo-server/src/lib/jrfs/simple-git.ts
@@ -1,15 +1,24 @@
-import { registerPlugin } from "@jrfs/core";
+import { command, registerPlugin } from "@jrfs/core";
 import registerGitPluginShared from "demo-shared/jrfs/git";
 // import { simpleGit } from "simple-git";
+
+const gitCommands = [
+  command("git.add", async function gitAdd(props, params) {
+    console.log("Yooooo git.add!");
+    return { files: ["OK!"] };
+  }),
+  command("git.commit", async function gitCommit(props, params) {
+    console.log("Yooooo git.commit!");
+    return { commit: "OK!" };
+  }),
+  command("git.push", async function gitPush(props, params) {
+    console.log("Yooooo git.push!");
+    return { commit: "OK!" };
+  }),
+];
 
 export default registerPlugin("git", function registerGitPlugin(params) {
   registerGitPluginShared.call(this, params);
   console.log("[GIT] Registering host command runners...");
-  if (params && typeof params !== "boolean") {
-    // params?.run?.["git.add"](null!, {});
-    // TODO: Set simpleGit options, baseDir, etc... from repo config!
-    // const sgit = simpleGit();
-    // CONSIDER: Maybe create the runners right here with sgit...
-    // this.registerCommandRunners(params);
-  }
+  this.commands.register(gitCommands);
 });
